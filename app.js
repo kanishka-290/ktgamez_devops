736261a87cb9c57d7dd9cf944ebaba266512b6a9
Aryan Chavda <er.aryan.chavda@gmail.com>
2022-05-17 09:39:29 +0530

    Static files

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const corsOptions = {
   credentials: true,
 }
 app.use(cors(corsOptions));
-app.use(express.static(__dirname + '/static'));
 app.get("/",function (req,res) {
     //res.send("Hello user");
     res.render("notfound.ejs")
