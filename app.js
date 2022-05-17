eaed6ed79d37f397d098d9161ef404d8330fc6b7
Aryan Chavda <er.aryan.chavda@gmail.com>
2022-05-17 17:41:28 +0530

    CORS policy

--- app.js
+++ app.js
@@ -8,18 +8,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 
-const whitelist = ["http://localhost:3001","http://localhost:3000"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions));
+
+
+app.use(cors({
+  origin: "*"
+}));
 
 
 app.get("/",function (req,res) {
