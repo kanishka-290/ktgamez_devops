007de67aa79eb628931742342b51497fb538dc4b
Aryan Chavda <er.aryan.chavda@gmail.com>
2022-05-31 17:52:53 +0530

    Admin login logout

--- controllers/admin/dashboard.js
+++ controllers/admin/dashboard.js
@@ -1,12 +1,84 @@
+const { validationResult, body } = require("express-validator");
+const md5 = require("md5");
+const sqlConnect = require("../../config/dbconnection");
+const store = require("store2")
+
+// Dashboard ########################
 const dashboard = (req,res) =>{
+    var user = store.get("user")
+    if(user == null || user == "" || user == undefined){
+        return res.redirect('../superadmin/login');
+    }
     res.render("admin/index.ejs",{ title : "GiroGamez Superadmin Panel - KT Gamez powered by GiroGames" });
 }
+
+
+// Login System ################################
 const login = (req,res) =>{
-    res.render("admin/login.ejs");
+    res.render("admin/login.ejs",{alert:[]});
 }
+const login_post = async (req,res) =>{
+    
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        const alert = errors.array()
+        return res.render("admin/login.ejs",{alert})
+    }
+
+    const connection = await sqlConnect()
+    var [result,field] = await connection.query("SELECT * FROM `super_admins` WHERE `email`='"+req.body.email+"' AND `password`='"+md5(req.body.password)+"'")
+    if(result.length===0){
+        alert = [
+            {
+                msg:"Invalid username or password"
+            }
+        ]
+        return res.render("admin/login.ejs",{alert})
+    }
+    await store.set("user",encodeURI(result[0].id))
+    res.redirect('/superadmin/');
+}
+
+// Logout ################################
+const logout = async (req,res) =>{
+    store.remove("user")
+    res.redirect("/superadmin/login");
+}
+
+// forgot password ################################
+const forgotpassword = (req,res) =>{
+    res.render("admin/forget-password.ejs");
+}
+
+// Genre Management ########################
 const grnrecreate = (req,res) =>{
-    res.render("admin/addgamegenre.ejs");
+    res.render("admin/addgamegenre.ejs",{alert:[]});
 }
+const grnrecreate_post = async (req,res) =>{
+    console.log(req.body)
+    const errors = validationResult(req);
+    console.log(errors)
+    if(!errors.isEmpty()){
+        const alert = errors.array()
+        return res.render("admin/addgamegenre.ejs",{alert})
+    }
+    const connection = await sqlConnect()
+
+    var [result,field] = await connection.query("SELECT `id` FROM `game_genres` WHERE `genre_name`='"+req.body.genre_name+"'")
+    if(result.length !== 0){
+        alert = [
+            {
+                msg:"Genre already exist"
+            }
+        ]
+        return res.render("admin/addgamegenre.ejs",{alert})
+    }
+    
+    await connection.query("INSERT INTO `game_genres` SET `genre_name`='"+req.body.genre_name+"',`genre_description`='"+req.body.description+"',`genre_status`='"+req.body.genre_status+"',`created_at`=CURRENT_TIMESTAMP(),`updated_at`=CURRENT_TIMESTAMP()")
+    res.redirect("genreview");
+}
+
 const genrelist = (req,res) =>{
     res.render("admin/viewgamegenre.ejs")
 }
@@ -36,9 +108,17 @@ const referandwin = (req,res) =>{
 }
 module.exports = {
     dashboard,
+
     login,
+    login_post,
+    logout,
+
+    forgotpassword,
+
     grnrecreate,
+    grnrecreate_post,
     genrelist,
+
     addgame,
     gameview,
     leaderboardgamecreate,
@@ -46,4 +126,5 @@ module.exports = {
     users,
     settings,
     referandwin,
+    
 }
--- routes/admin/dashboard.js
+++ routes/admin/dashboard.js
@@ -1,11 +1,43 @@
 const admin = require("../../controllers/admin/dashboard");
-
+const {check,validationResult} = require("express-validator");
+const emailValidator = require("email-validator");
+const { min } = require("moment");
 
 module.exports =function(app) {
     
    app.get("/superadmin/", admin.dashboard)
+
    app.get("/superadmin/login", admin.login);
+   app.post("/superadmin/login", [
+      check("email","Please enter valid email address")
+         .exists()
+         .isLength({min:5}),
+      check("email","Please enter valid email address")
+         .isEmail(),
+      check("password","Please enter valid password")
+         .isLength({min:5,max:40})
+   ], admin.login_post);
+   app.get("/superadmin/logout", admin.logout);
+
+
+
+   app.get("/forgot-password", admin.forgotpassword)
+
+   
+
    app.get("/superadmin/genrecreate", admin.grnrecreate)
+   app.post("/superadmin/genrecreate",[
+      check("genre_name","Genre name is required")
+         .exists()
+         .isLength({max:30,min:2}),
+      check("description","Description is required")
+         .exists()
+         .isLength({max:300,min:2}),
+      check("genre_status","Genre status is required")
+         .exists()
+   ], admin.grnrecreate_post)
+
+
    app.get("/superadmin/genreview", admin.genrelist)
    app.get("/superadmin/gamecreate", admin.addgame)
    app.get("/superadmin/game", admin.gameview)
